fe847ca46bc2aafa76c9bd0fcdd5435cbe5c2cc9
szokolimatyas <56924108+szokolimatyas@users.noreply.github.com>
2023-05-07 18:22:49 +0200

    parse all types

--- src/Squasher/Types.hs
+++ src/Squasher/Types.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE LambdaCase #-}
+
 module Squasher.Types where
 
 import Data.Text(Text)
@@ -9,7 +11,9 @@ import qualified Data.Set as Set
 import Data.Data
 import Data.Map(Map)
 import Data.List (intercalate)
+import qualified Data.Maybe as Maybe
 import Foreign.Erlang.Term
+import qualified Data.Map as Map
 
 
 data ErlType = EInt
@@ -41,12 +45,26 @@ data ErlType = EInt
 instance FromTerm ErlType where
     fromTerm t = case t of
         (Atom _ "integer") -> Just EInt
+        (Atom _ "float") -> Just EFloat
         (Tuple [Atom _ "atom", Atom _ value]) -> Just $ ENamedAtom value
-        (Tuple [Atom _ "union", List terms Nil]) -> EUnion . Set.fromList <$> mapM fromTerm terms
-        (Tuple [Atom _ "func", List args Nil, res]) -> EFun <$> mapM fromTerm args <*> fromTerm res
         (Tuple [Atom _ "tuple", List terms Nil]) -> ETuple <$> mapM fromTerm terms
+        (Atom _ "any") -> Just EAny
+        (Tuple [Atom _ "union", List terms Nil]) -> EUnion . Set.fromList <$> mapM fromTerm terms
+        (Tuple [Atom _ "function", List args Nil, res]) -> EFun <$> mapM fromTerm args <*> fromTerm res
         (Atom _ "unknown") -> Just EUnknown
+        (Atom _ "binary") -> Just EBinary
+        (Atom _ "bitstring") -> Just EBitString
+        (Tuple [Atom _ "list", List terms Nil]) -> 
+            Just $ EList $ Set.fromList $ Maybe.mapMaybe fromTerm terms
+        (Tuple [Atom _ "map", List terms Nil]) -> 
+            Just $ EMap $ Map.fromList $ Maybe.mapMaybe 
+                (\case { Tuple [t1,t2] -> (,) <$> fromTerm t1 <*> fromTerm t2; _ -> Nothing}) terms
+        (Atom _ "pid") -> Just EPid
+        (Atom _ "port") -> Just EPort
+        (Atom _ "reference") -> Just ERef
+        (Atom _ "boolean") -> Just EBoolean
         _ -> Nothing
+
 
 instance Show ErlType where
     show ty = case ty of
@@ -68,5 +86,5 @@ instance Show ErlType where
         EPort -> "port()"
         ERef -> "reference()"
         EBoolean -> "boolean()"
-        EMap _ -> "map()"
-        EList _ -> "list()" 
+        EMap ts -> "#{" ++ intercalate ", " (map (\(t1, t2) -> show t1 ++ " => " ++ show t2) (Map.toList ts)) ++ "}"
+        EList ts -> "list(" ++ intercalate "| " (map show $ Set.toList ts) ++ ")" 
