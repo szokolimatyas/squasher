f810aca35c780343caa16c2ad168ddbf2b0af4ff
szokolimatyas <56924108+szokolimatyas@users.noreply.github.com>
2023-12-03 15:10:02 +0100

    more permissive local squash

--- src/Squasher/Local.hs
+++ src/Squasher/Local.hs
@@ -19,12 +19,16 @@ shouldMerge (ETuple (ENamedAtom n : elems) : ts) =
         filt :: ErlType -> Bool
         filt (ETuple (ENamedAtom n' : elems')) =
             n == n' && length elems == length elems'
+        filt (EUnion ts') = any filt ts'
         filt _ = False
 shouldMerge (ENamedAtom n : ts) =
     all filt ts where
         filt :: ErlType -> Bool
         filt (ENamedAtom n') = n == n'
+        filt (EUnion ts')    = any filt ts'
         filt _               = False
+shouldMerge (EUnion ts' : ts) =
+    any (\t' -> shouldMerge (t':ts)) ts'
 shouldMerge _ = False
 
 aliasTuple :: SquashConfig -> ErlType -> (SquashConfig, ErlType)
@@ -39,7 +43,7 @@ squash :: SquashConfig -> [Int] -> IntSet -> SquashConfig
 squash conf []     _d = conf
 squash conf (a1:w) d  = squash conf' (w ++ IntSet.toList as) (IntSet.insert a1 d) where
     as = aliases (lookupAlias a1 conf) IntSet.\\ d
-    ap = Debug.Trace.trace ((show a1) ++ " " ++ (show $ (IntSet.delete a1 d) <> as)) $ IntSet.delete a1 d
+    ap = IntSet.delete a1 d
     -- refers to ai, what is it???
     f delta a2 =
         if not (shouldMerge (map (resolve delta) [EAliasMeta a1, EAliasMeta a2]))
@@ -59,5 +63,5 @@ squashLocal opts tEnv = Map.foldlWithKey h initialConf (unTyEnv tEnv) where
     initialConf = SquashConfig (MkAliasEnv IntMap.empty 0) (MkTyEnv Map.empty) opts
 
     h conf n t = addFunction n t1 conf2 where
-        (conf1, t1) = Debug.Trace.trace (show $ aliasMap $ aliasEnv $ fst $ aliasTuple conf t) aliasTuple conf t
+        (conf1, t1) = aliasTuple conf t
         conf2 = squashAll conf1 t1
